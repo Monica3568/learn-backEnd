682799888c44ec4121818c7872ada1c12abddf8e
Monica <305064132@qq.com>
2022-07-28 00:03:41 +0800

    新增

--- service/service-edu/src/main/java/com/meiqi/edu/entity/EduTeacher.java
+++ service/service-edu/src/main/java/com/meiqi/edu/entity/EduTeacher.java
@@ -22,7 +22,7 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @ApiModel(value="EduTeacher对象", description="讲师")
-public class Teacher implements Serializable {
+public class EduTeacher implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
